89e761aa97e9dc35dfb89a72ab5a0e91bc84e0b8
yuhyeonjo <jo.yuhyeon16@gmail.com>
2023-08-11 04:48:00 +0900

    MVC 패턴 적용

---
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberListServlet.java
@@ -0,0 +1,33 @@
+package hello.servlet.web.servletmvc;
+
+import hello.servlet.domain.Member;
+import hello.servlet.domain.MemberRepository;
+import org.springframework.cglib.proxy.Dispatcher;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
+public class MvcMemberListServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        List<Member> members = memberRepository.findAll();
+
+        // Model에 데이터를 보관
+        request.setAttribute("members", members);
+
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
